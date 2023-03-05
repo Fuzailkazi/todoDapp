5156a505161c3a72493357f690b1991dbf408cbe
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2023-03-05 13:58:30 +0530

    alchemy and metamask setup done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_KEY,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+  },
 };
